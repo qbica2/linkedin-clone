4b650e33654229271cb65d64cb69986a822ac4b6
kubilay <kubidik27@gmail.com>
2022-02-18 17:07:21 +0300

    sidebar done

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -6,12 +6,30 @@ function Sidebar() {
   return (
     <div className={style.sidebar}>
         <div className={style.top}>
-                <img src="" alt="" />
+                <img src="https://picsum.photos/200/300" alt="" />
                 <Avatar className={style.avatar}/>
                 <h2 >Kubilay Akdemir</h2>
                 <h4>Junior Frontend Developer</h4>
+                <div className={style.connections}>
+                    <div className={style.connectionsTop}>
+                        <h4>Connections</h4>
+                        <p>13</p>
+                    </div>
+                    <div className={style.connectionsTop}> 
+                    <h3>Grow your network</h3>
+                    </div>
+                </div>
         </div>
-        <div className={style.bottom}>  </div>
+        <div className={style.bottom}> 
+            <div className={style.bottomTop}>
+               <a href="#">Groups</a>
+               <a href="#">Events</a>
+               <a href="#">Followed Hashtags</a>
+            </div>
+            <div className={style.bottomBot}>
+                Discover More
+            </div>
+         </div>
     </div>
   )
 }
